4607b14d250b5ad6f1adb7612a2fcc221f7e2ae3
nataliakostyrko <nataliskrip1408@gmail.com>
2024-03-29 12:44:31 +0100

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,17 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+function filterArray(numbers, value) {
 
-    return contentWidth + paddingWidth * 2 + borderWidth * 2;
+    let filteredArray = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            filteredArray.push(numbers[i]);
+        }
+    }    
+    return filteredArray;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
